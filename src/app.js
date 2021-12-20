afd02ddc617ea7b0f17692ee941319e0f390ebf5
Duy Hung <daoduyhung0000@gmail.com>
2021-12-20 22:14:09 +0700

    update

--- src/app.js
+++ src/app.js
@@ -1,26 +1,19 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
 import Main from "./components/Main/main";
 import { AddChild, New, tailwindAdd } from "./Javascript/tool";
-import { auth } from "./Javascript/firebase";
-import Login from "./components/Log In/LogIn";
+import SignInAndSignUp from "./components/SignIn&SignUp/LogIn";
 
 class App {
     constructor(){
         this.box = New('div')
-        this.signIn = new Login
-        this.signUp = null 
+        this.signIn_SignUp_box = new SignInAndSignUp()
         this.mainApp = new Main()
 
         this.boxStyle = ['w-full', 'h-screen']
 
         tailwindAdd(this.boxStyle, this.box)
     }
-    goMain(){
-        this.box.children[0].remove
-        AddChild(this.box, this.mainApp.render())
-    }
     render(){
-        AddChild(this.box, this.signIn.render(this.goMain.bind(this)))
+        AddChild(this.box, this.signIn_SignUp_box.render())
         return this.box
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'tailwindcss/tailwind.css'
 import { AddChild, Element } from './Javascript/tool'
 import App from './app'
-import Login from './components/Log In/LogIn';
+
 let currentApp = new App();
 (()=>{
     AddChild(Element('#root'), currentApp.render())
